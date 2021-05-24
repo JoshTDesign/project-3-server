ef5c2eb2bb1e11eb96ca9eb4f9be3dea00f09785
Trevor Eckberg <trevor.eckberg@gmail.com>
2021-05-24 14:34:24 -0700

    fixed stuffs

--- server.js
+++ server.js
@@ -6,22 +6,22 @@ const cors = require('cors');
 //const helpers = require('./utils/helpers');
 
 const sequelize = require('./config/connection');
-const SequelizeStore = require('connect-session-sequelize')(session.Store);
+//const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-const sess = {
-  secret: 'Super secret secret',
-  cookie: {},
-  resave: false,
-  saveUninitialized: true,
-  store: new SequelizeStore({
-    db: sequelize
-  })
-};
+// const sess = {
+//   secret: 'Super secret secret',
+//   cookie: {},
+//   resave: false,
+//   saveUninitialized: true,
+//   store: new SequelizeStore({
+//     db: sequelize
+//   })
+// };
 
-app.use(session(sess));
+// app.use(session(sess));
 app.use(routes);
 app.use(cors());
 app.use(express.json());
