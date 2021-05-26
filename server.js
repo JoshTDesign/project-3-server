12e8a1a93b86fd23133cf54c50eaedc464445b86
Josh Taylor <josh@joshtaylordesign.com>
2021-05-26 13:55:55 -0700

    changes

--- server.js
+++ server.js
@@ -25,9 +25,9 @@ const PORT = process.env.PORT || 3001;
 app.use(routes);
 app.use(cors());
 app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
-sequelize.sync({ force: false }).then(() => {
+sequelize.sync({ force: true }).then(() => {
   app.listen(PORT, () => console.log('Now listening on PORT 3001'));
 });
