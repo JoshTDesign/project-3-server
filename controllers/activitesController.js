c83627327a17aba04c275b640a9a8f103247f456
Andrew Yu <igotyuu421@gmail.com>
2021-05-27 01:19:45 -0700

    avtivites controller

---
+++ controllers/activitesController.js
@@ -0,0 +1,37 @@
+const db = require("../models/Activity");
+
+// Defining methods for the booksController
+module.exports = {
+  findAll: function(req, res) {
+    db.Activity
+      .find(req.query)
+      .sort({ date: -1 })
+      .then(dbModel => res.json(dbModel))
+      .catch(err => res.status(422).json(err));
+  },
+  findById: function(req, res) {
+    db.Activity
+      .findById(req.params.id)
+      .then(dbModel => res.json(dbModel))
+      .catch(err => res.status(422).json(err));
+  },
+  create: function(req, res) {
+    db.Activity
+      .create(req.body)
+      .then(dbModel => res.json(dbModel))
+      .catch(err => res.status(422).json(err));
+  },
+  update: function(req, res) {
+    db.Activity
+      .findOneAndUpdate({ id: req.params.id }, req.body)
+      .then(dbModel => res.json(dbModel))
+      .catch(err => res.status(422).json(err));
+  },
+  remove: function(req, res) {
+    db.Activity
+      .findById({ id: req.params.id })
+      .then(dbModel => dbModel.remove())
+      .then(dbModel => res.json(dbModel))
+      .catch(err => res.status(422).json(err));
+  }
+};
