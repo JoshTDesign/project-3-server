92b32cf2078954db09f1d19a68bd0a8d55a5f164
Andrew Yu <igotyuu421@gmail.com>
2021-05-27 10:47:52 -0700

    updated

--- controllers/tripsController.js
+++ controllers/tripsController.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 const express = require('express');
 const router = express.Router();
 const {Trip, Activity} = require('../models');
@@ -95,44 +94,3 @@ router.delete("/:id",tokenAuth,(req,res)=>{
 })
 
 module.exports = router;
-=======
-const db = require('../models');
-
-
-module.exports = {
-
-    findAll: function(req, res) {
-        db.Trip
-        .find(req.query)
-        .sort({start_date: -1})
-        .then(foundDb => res.json(foundDb))
-        .catch(err => res.status(400).json(err));
-    },
-    findById: function(req, res) {
-        db.Trip
-          .findById(req.params.id)
-          .then(foundDb => res.json(foundDb))
-          .catch(err => res.status(422).json(err));
-      },
-      create: function(req, res) {
-        db.Trip
-          .create(req.body)
-          .then(foundDb => res.json(foundDb))
-          .catch(err => res.status(422).json(err));
-      },
-      update: function(req, res) {
-        db.Trip
-          .findOneAndUpdate({ _id: req.params.id }, req.body)
-          .then(foundDb => res.json(foundDb))
-          .catch(err => res.status(422).json(err));
-      },
-      remove: function(req, res) {
-        db.Trip
-          .findById({ _id: req.params.id })
-          .then(foundDb => foundDb.remove())
-          .then(foundDb => res.json(foundDb))
-          .catch(err => res.status(422).json(err));
-      }
-
-};
->>>>>>> develop
